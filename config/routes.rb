fa1f35b9888fe057e7c9314ec9f2a2be1ab6e6f2
Laszlo-VSC <laszloluque@gmail.com>
2022-05-23 15:54:56 +0200

    mailer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :friends
+  resource :contact
   root 'home#index'
   get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
